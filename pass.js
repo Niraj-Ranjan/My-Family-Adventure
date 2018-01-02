fbf7a123575b9ea1d175f43b494a08cd9bc34471
Niraj Ranjan <32240290+Niraj-Ranjan@users.noreply.github.com>
2018-01-03 02:25:25 +0530

    MyFamilyAdventure

---
+++ pass.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+var password = "please";
+var x = prompt("Enter in the password "," ");
+if (x.toLowerCase() == password) {
+  alert("Come right in \n \n You've entered in the right password");
+  window.location = "sample.html";
+}
+else {
+  window.location = "bad.htm";
+}
+</script>
